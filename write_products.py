75d25097e7654643be3e2e3a2b80f562df2aa246
Soichi Hayashi <soichih@gmail.com>
2016-07-19 00:17:31 +0000

    updated write_products.py

--- write_products.py
+++ write_products.py
@@ -6,12 +6,27 @@ import os
 import glob
 import json
 
-files = []
+tracks = [] 
 #product = {"type"=>"soichih/neuro-mif/lmax", "files"=>[]}
 
-for file in glob.glob("lmax*.mif"):
-    files.append({"filename":file, "size":os.path.getsize(file)})
+for filename in glob.glob("output.*.tck"):
+    size = os.path.getsize(filename)
+    #files.append({"filename":file, "size":os.path.getsize(file)})
+    tokens = filename.split(".")
+    #['output', '5', 'SD_PROB', '4', 'tck']
+    print tokens
+    track=int(tokens[1])
+    type=tokens[2]
+    lmax=int(tokens[3])
+
+    while len(tracks) <= track:
+        tracks.append({})
+
+    if not type in tracks[track]:
+        tracks[track][type] = {}
+
+    tracks[track][type][lmax] = {"filename":filename, "size":size}
 
 with open('products.json', 'w') as pfile:
-    json.dump([{"type": "soichih/neuro-mif/lmax", "files": files}], pfile)
+    json.dump([{"type": "soichih/neuro-mif/tracking", "tracks": tracks}], pfile)
 
