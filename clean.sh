c61290a0023c5f24fdcfc4d2d5e087671688a0d8
Soichi Hayashi <hayashis@iu.edu>
2016-10-16 12:15:11 -0400

    added timer

--- clean.sh
+++ clean.sh
@@ -1,8 +1,13 @@
-rm neuro-tracking.prep.e*
-rm neuro-tracking.prep.o*
+rm neuro-tracking.prep.*
+rm neuro-tracking.final.*
+rm neuro-tracking.lmax.*
 rm finished
 rm jobid*
+rm final_jobid
 rm jq
 rm response.txt
 rm *.mif
 rm grad.b
+rm *.tck
+rm products.json
+rm wm.nii.gz
